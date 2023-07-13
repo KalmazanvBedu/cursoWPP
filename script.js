7c1505931686423994fac585ec59435b023de19a
KalmazanBedu <qacalidadalmazan@gmail.com>
2023-07-13 17:57:21 -0600

    Se vuelve a implementar GTM

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var button = document.getElementById("button");
 
-button.addEventListener('click',function(){
-    gtag('event', 'mostrar_saludo');
-});
+// button.addEventListener('click',function(){
+//     gtag('event', 'mostrar_saludo');
+// });
